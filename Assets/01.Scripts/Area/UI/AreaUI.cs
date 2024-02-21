2cbb1b78778d23428ae46f586fe60590b477a7cc
DDongYeop <ddongyeop0129@gmail.com>
2024-02-21 22:20:04 +0900

    UI

---
+++ Assets/01.Scripts/Area/UI/AreaUI.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaUI : MonoBehaviour
+{
+    [Header("Data")] 
+    private AreaData _currentAreaData; 
+    
+    [Header("UI")]
+    private Image _image;
+    private TextMeshProUGUI _cntText;
+    
+    private void OnEnable()
+    {
+        _image = GetComponent<Image>();
+        _cntText = transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SetUp(AreaData areaData, Sprite sprite, int cnt)
+    {
+        _currentAreaData = areaData;
+        _image.sprite = sprite;
+        _cntText.text = cnt.ToString();
+    }
+
+    public void UseArea(int cnt)
+    {
+        _cntText.text = cnt.ToString();
+    }
+    
+    public void ObjectSelect()
+    {
+        // 선택
+    }
+}
